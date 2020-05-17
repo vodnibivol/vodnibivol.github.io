9ca2374b108c62b37e9bb1c4f37f88281b0dfb5b
Filip <terragnox@gmail.com>
2020-05-17 04:39:31 +0200

    added info bar

--- beseda_dneva/js/script.js
+++ beseda_dneva/js/script.js
@@ -1,4 +1,15 @@
-contentsDiv = document.getElementById('contents');
+contentsDiv = document.getElementById("contents");
+infoButton = document.getElementById('info');
+message = document.getElementById('message');
+
+infoButton.addEventListener('click', () => {message.classList.toggle('show')});
+
+function gabi_content(element) {
+  // var element = document.getElementById('txt');
+  text = element.innerText || element.textContent;
+
+  console.log(text);
+}
 
 function randInt(num) {
   return Math.floor(Math.random() * num);
@@ -6,25 +17,35 @@ function randInt(num) {
 
 function displayText(obj) {
   objLength = Object.values(obj).length;
-  
-  randomEntry = Object.values(obj)[randInt(objLength) - 1]
-  contentsDiv.innerHTML = randomEntry;
+
+  randomEntry = Object.values(obj)[randInt(objLength) - 1];
+
+  console.log(randomEntry.length);
+  gabi_content(randomEntry);
+
+  if (randomEntry.length < 2000) {
+    contentsDiv.innerHTML = randomEntry;
+  } else {
+    displayText(obj);
+  }
 }
 
 /* ------ HTTP Request ------- */
 
-var xmlhttp = new XMLHttpRequest();
-var url = `CHUNKS/${randInt(98)}.json`;
+function jsonRequest() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = `CHUNKS/${randInt(98)}.json`;
 
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    
-    displayText(myObj);
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+
+      displayText(myObj);
     }
-};
+  };
 
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
 
-// contentsDiv.innerHTML = "<h1>header</h1>"
+jsonRequest();
