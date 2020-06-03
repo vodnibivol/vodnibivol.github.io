20956f211756e83c3d9d82013777315ba6c5513d
Filip <terragnox@gmail.com>
2020-06-03 22:17:02 +0200

    enabled sound on click

--- vremenska_napoved/script.js
+++ vremenska_napoved/script.js
@@ -1,7 +1,10 @@
 let date;
 let text;
+let playing = false;
 
 let audioSequence = [];
+
+document.getElementById("content").addEventListener("click", playSound);
 
 function parseResponse(data) {
   const dateText = document.getElementById("dateText");
@@ -12,8 +15,6 @@ function parseResponse(data) {
 
   dateText.innerHTML = toMorse(date);
   paraText.innerHTML = toMorse(text);
-
-  // document.addEventListener("click", playSound);
 }
 
 function jsonRequest() {
@@ -37,47 +38,44 @@ function toMorse(string) {
   const c = "-";
 
   morse = {
-    a: p + c,
-    b: c + p + p + p,
-    c: c + p + c + p,
-    č: c + p + c + p,
-    d: c + p + p,
-    e: p,
-    f: p + p + c + p,
-    g: c + c + p,
-    h: p + p + p + p,
-    i: p + p,
-    j: p + c + c + c,
-    k: c + p + c,
-    l: p + c + p + p,
-    m: c + c,
-    n: c + p,
-    o: c + c + c,
-    p: p + c + c + p,
-    r: p + c + p,
-    s: p + p + p,
-    š: p + p + p,
-    t: c,
-    u: p + p + c,
-    v: p + p + p + c,
-    z: c + c + p + p,
-    ž: c + c + p + p,
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    č: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    r: ".-.",
+    s: "...",
+    š: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    z: "--..",
+    ž: "--..",
     " ": "/",
-    0: c + c + c + c + c,
-    1: p + c + c + c + c,
-    2: p + p + c + c + c,
-    3: p + p + p + c + c,
-    4: p + p + p + p + c,
-    5: p + p + p + p + p,
-    6: c + p + p + p + p,
-    7: c + c + p + p + p,
-    8: c + c + c + p + p,
-    9: c + c + c + c + p,
-    ".": p + c + p + c + p + c,
-    ",": c + c + p + p + c + c,
-    start: c + p + c + p + c,
-    end: p + p + p + c + p + c,
-    err: p + p + p + p + p + p + p + p,
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
   };
 
   list = [];
@@ -108,28 +106,35 @@ jsonRequest();
 /* ---- audio ---- */
 
 function playSound() {
-  console.log(audioSequence);
+  if (!playing) {
+    // initialize
+    playing = true;
 
-  var ctx = new AudioContext();
-
-  var i = 0;
-  var duration;
+    var ctx = new AudioContext(),
+      i = 0,
+      duration;
+  } else if (playing) {
+    playing = false;
+  }
 
   function beep() {
     function sound(duration) {
-      len = duration / 1000;
+      var osc = ctx.createOscillator(),
+        g = ctx.createGain();
 
-      var osc = ctx.createOscillator();
-      var g = ctx.createGain();
       osc.connect(g);
       g.connect(ctx.destination);
-      g.gain.value = 0.2;
+
+      osc.frequency.setValueAtTime(600, ctx.currentTime);
+      g.gain.setValueAtTime(0, ctx.currentTime);
 
       osc.start();
-      // g.gain.setValueAtTime(0, ctx.currentTime);
-      // g.gain.linearRampToValueAtTime(.1, ctx.currentTime + len / 2);  // in sec
-      // g.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + len);  // in sec
-      osc.stop(ctx.currentTime + len);
+
+      g.gain.setTargetAtTime(0.2, ctx.currentTime, 0.01); // in sec
+      setTimeout(function () {
+        g.gain.setTargetAtTime(0, ctx.currentTime, 0.01);
+        osc.stop(ctx.currentTime + 0.1);
+      }, duration);
     }
     if (audioSequence[i] == ".") {
       duration = 100; // in ms
@@ -147,10 +152,8 @@ function playSound() {
 
     i++;
 
-    if (i != audioSequence.length) {
-      setTimeout(function () {
-        beep();
-      }, duration + 20);
+    if (i != audioSequence.length && playing) {
+      setTimeout(beep, duration + 20);
     }
   }
   beep();
