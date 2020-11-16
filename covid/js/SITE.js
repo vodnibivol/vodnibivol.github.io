82295a29430a83cbe2d76c46efabea8ba52ec589
vodnibivol <terragnox@gmail.com>
2020-11-16 14:43:48 +0100

    covid: update
    upgrade from gov.si to sledilnik.org

--- covid/js/SITE.js
+++ covid/js/SITE.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict';
 
 /* -------- variable declarations -------- */
 
-const infoButton = document.getElementById("infoButton");
-const infoBubble = document.getElementById("infoBubble");
-const settingsButton = document.getElementById("settingsButton");
-const settingsBubble = document.getElementById("settingsBubble");
-const sizeToggle = document.getElementById("sizeToggle");
-const chartDiv1 = document.getElementById("chartDiv1");
-const chartDiv2 = document.getElementById("chartDiv2");
-const card = document.getElementById("card");
-const spinner = document.getElementById("spinner");
+const infoButton = document.getElementById('infoButton');
+const infoBubble = document.getElementById('infoBubble');
+const settingsButton = document.getElementById('settingsButton');
+const settingsBubble = document.getElementById('settingsBubble');
+const sizeToggle = document.getElementById('sizeToggle');
+const chartDiv1 = document.getElementById('chartDiv1');
+const chartDiv2 = document.getElementById('chartDiv2');
+const card = document.getElementById('card');
+const spinner = document.getElementById('spinner');
 
-const url = "https://www.gov.si/assets/vlada/Koronavirus-podatki/COVID-19-vsi-podatki.xlsx";
-const proxy = "https://cors-anywhere.herokuapp.com/";
+const url = 'https://api.sledilnik.org/api/stats';
 
-const filename = "COVID-19-vsi-podatki.xlsx";
+const filename = 'stats.json';
 
 /* ---------- load preferences ---------- */
 
@@ -23,43 +22,43 @@ let settings;
 
 // first time visit: set default
 if (chartDiv1.offsetWidth < 600) {
-  settings = { data: "small" };
+  settings = { data: 'small' };
 } else {
-  settings = { data: "large" };
+  settings = { data: 'large' };
 }
 
 if (localStorage.covidSettings) {
   settings = JSON.parse(localStorage.covidSettings);
 }
 
-if (settings.data == "small") {
+if (settings.data == 'small') {
   sizeToggle.checked = true;
 }
 
 /* ----------- event listeners ----------- */
 
-infoButton.addEventListener("click", () => {
-  infoBubble.classList.toggle("open");
+infoButton.addEventListener('click', () => {
+  infoBubble.classList.toggle('open');
 
-  if (infoBubble.classList.contains("open")) {
+  if (infoBubble.classList.contains('open')) {
     countdown = setTimeout(() => {
-      infoBubble.classList.remove("open");
+      infoBubble.classList.remove('open');
     }, 5000);
   } else {
     clearTimeout(countdown);
   }
 });
 
-settingsButton.addEventListener("click", () => {
-  settingsBubble.classList.toggle("open");
-  settingsButton.classList.toggle("reverse");
+settingsButton.addEventListener('click', () => {
+  settingsBubble.classList.toggle('open');
+  settingsButton.classList.toggle('reverse');
 });
 
-sizeToggle.addEventListener("change", () => {
+sizeToggle.addEventListener('change', () => {
   if (sizeToggle.checked) {
-    settings.data = "small";
+    settings.data = 'small';
   } else {
-    settings.data = "large";
+    settings.data = 'large';
   }
 
   localStorage.covidSettings = JSON.stringify(settings);
@@ -75,69 +74,34 @@ sizeToggle.addEventListener("change", () => {
 
 /* -------- function declarations -------- */
 
-function getFile() {
-  /* set up async GET request */
-  var req = new XMLHttpRequest();
-  req.open("GET", proxy + url, true); // proxy + url
-  req.responseType = "arraybuffer";
+async function getFile() {
+  try {
+    let response = await fetch(url);
+    let parsedJson = await response.json();
 
-  req.onload = function (e) {
-    var data = new Uint8Array(req.response);
-    var workbook = XLSX.read(data, { type: "array" });
+    spinner.classList.add('hidden');
+    chartDiv1.classList.remove('hidden');
+    chartDiv2.classList.remove('hidden');
+    card.classList.remove('hidden');
 
-    /* DO SOMETHING WITH workbook HERE */
-    let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-
-    parseData(worksheet);
-    spinner.classList.add("hidden");
-    chartDiv1.classList.remove("hidden");
-    chartDiv2.classList.remove("hidden");
-    card.classList.remove("hidden");
-  };
-
-  req.send();
+    parseData(parsedJson);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 let dates, dates_s, daily_tested, daily_positive, daily_positive_s, daily_percent, daily_percent_s;
 
-function parseData(worksheet) {
-  let getColumn = function (sheet, col_num) {
-    let range = XLSX.utils.decode_range(sheet["!ref"]);
+function parseData(obj) {
+  console.log(obj);
 
-    let last_row = range.e.r;
-    let last_column = range.e.c;
+  if (!obj[obj.length - 1].cases.confirmedToday) {
+    obj = obj.slice(0, -1)
+  }
 
-    let column = [];
-
-    for (var rowNum = 1; rowNum <= last_row; rowNum++) {
-      let nextCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: col_num })];
-      if (typeof nextCell === "undefined") {
-        // column.push(void 0);
-        return column;
-      } else if (nextCell.w.toLowerCase().includes("opombe")) {
-        return column;
-      } else column.push(nextCell.w);
-    }
-    return column;
-  };
-
-  dates = getColumn(worksheet, 0);
-  daily_tested = getColumn(worksheet, 2);
-  daily_positive = getColumn(worksheet, 4);
-
-  dates = dates.map((date) => {
-    // wrong date correction
-    if (date.split(" ").length != 3) {
-      console.log("wrong syntax : " + date);
-
-      date = date.split(".").join(". ");
-    }
-
-    date = date.replaceAll("/", ".");
-    date = date.replace(" 2020", "");
-
-    return date;
-  });
+  dates = obj.map((item) => `${item.day}. ${item.month}`);
+  daily_tested = obj.map((item) => item.tests.performed.today || item.tests.performed.toDate);
+  daily_positive = obj.map((item) => item.cases.confirmedToday || 0);
 
   daily_percent = (function () {
     let arr = [];
@@ -166,29 +130,29 @@ function parseData(worksheet) {
 }
 
 function setCard(dates, daily_positive) {
-  const arrow = document.querySelector(".arrow");
-  const cardLower = document.querySelector(".card-lower");
-  const lastDate = document.querySelector(".last-date");
-  const lastPositive = document.querySelector(".last-positive");
+  const arrow = document.querySelector('.arrow');
+  const cardLower = document.querySelector('.card-lower');
+  const lastDate = document.querySelector('.last-date');
+  const lastPositive = document.querySelector('.last-positive');
 
   function writeDate() {
-    let date = dates[dates.length - 1].replace(".", "").split(" ");
+    let date = dates[dates.length - 1].replace('.', '').split(' ');
 
     let lastDay = date[0];
 
     let months = [
-      "januar",
-      "februar",
-      "marec",
-      "april",
-      "maj",
-      "junij",
-      "julij",
-      "avgust",
-      "september",
-      "oktober",
-      "november",
-      "december",
+      'januar',
+      'februar',
+      'marec',
+      'april',
+      'maj',
+      'junij',
+      'julij',
+      'avgust',
+      'september',
+      'oktober',
+      'november',
+      'december',
     ];
 
     let lastMonth = months[date[1] - 1];
@@ -204,10 +168,10 @@ function setCard(dates, daily_positive) {
 
     switch (lastPositiveNum.length) {
       case 4:
-        lastPositive.style["font-size"] = "80px";
+        lastPositive.style['font-size'] = '80px';
         break;
       case 5:
-        lastPositive.style["font-size"] = "65px";
+        lastPositive.style['font-size'] = '65px';
         break;
     }
   }
@@ -217,11 +181,11 @@ function setCard(dates, daily_positive) {
     let yesterday = parseInt(daily_positive[daily_positive.length - 2]);
 
     if (today < yesterday) {
-      arrow.src = "img/arrow_down.svg";
-      cardLower.style.backgroundColor = "#adf1ad";
+      arrow.src = 'img/arrow_down.svg';
+      cardLower.style.backgroundColor = '#adf1ad';
     } else {
-      arrow.src = "img/arrow_up.svg";
-      cardLower.style.backgroundColor = "#f1adad";
+      arrow.src = 'img/arrow_up.svg';
+      cardLower.style.backgroundColor = '#f1adad';
     }
   }
 
