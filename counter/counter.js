152a3fab301bba798c685f44d3d740de803e5fac
vodnibivol <vodnibivol@gmail.com>
2023-02-23 10:54:17 +0100

    refactored counter

--- counter/counter.js
+++ counter/counter.js
@@ -1,11 +1,11 @@
-const $ = (sel) => document.querySelector(sel);
-
 class Counter {
   constructor(options = {}) {
     this.width = options.width;
-    if (!this.width) throw new Error('width must be provided');
     this.counterEl = document.querySelector(options.el);
-    if (!this.counterEl) throw new Error('element must be provided');
+
+    if (!this.width) throw new Error('counter width must be provided');
+    if (!this.counterEl) throw new Error('counter element must be provided');
+
     this.init();
   }
 
@@ -21,26 +21,22 @@ class Counter {
   }
 
   render() {
-    const ch = [...this.counterEl.querySelectorAll('.num')];
-    const numString = this.num.toString();
+    const cells = [...this.counterEl.querySelectorAll('.num')];
+    const numString = '' + this.num;
 
-    const oneOffset = ch[0].offsetHeight / 10;
+    const cellHeight = cells[0].offsetHeight / 10;
 
-    for (let i = 0; i < ch.length; ++i) {
-      // from the back to the front!
-      const el = ch.at(-1 - i);
-      const val = numString.at(-1 - i);
-
-      const endOffset = (parseInt(val) || 0) * oneOffset;
-      el.style.transform = `translate3d(0, ${-endOffset}px, 0)`;
+    for (let i = 0; i < cells.length; ++i) {
+      const offset = numString.at(-i - 1) * cellHeight;
+      cells.at(-i - 1).style.transform = `translate3d(0, -${offset}px, 0)`;
     }
   }
 
   init() {
-    const html = `\
-    <div class="num-window flex-center">
+    const html = `
+    <div class="num-cell flex-center">
       <span class="num">0123456789</span>
     </div>`;
-    this.counterEl.innerHTML = new Array(this.width).fill(html).join('\n');
+    this.counterEl.innerHTML = new Array(this.width).fill(html).join('');
   }
 }
